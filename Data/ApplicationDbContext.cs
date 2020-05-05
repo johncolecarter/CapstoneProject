41012ed241fc6f38f73dc0a0b17470688c2f572f
johncolecarter <colecarter7777@gmail.com>
2020-05-05 10:45:32 -0500

    init

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper.Configuration;
+using capstone.Models;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace capstone.Data
+{
+
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public IConfiguration Configuration { get; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+
+        }
+        public ApplicationDbContext() : base(new DbContextOptionsBuilder<ApplicationDbContext>().UseSqlite("Data Source=app.db").Options,null)
+        {
+
+        }
+    }
+}
