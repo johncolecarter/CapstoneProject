fc3e5ec16ac04a669f14a2a79b33bda91253ba9d
johncolecarter <colecarter7777@gmail.com>
2020-05-19 09:34:53 -0500

    create fence styling

--- ClientApp/src/app/create-fence/create-fence.component.ts
+++ ClientApp/src/app/create-fence/create-fence.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { IFence } from '../interfaces/ifence';
 import { FenceService } from '../services/fence.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-fence',
@@ -11,16 +12,28 @@ export class CreateFenceComponent implements OnInit {
 
   public fences: IFence[];
 
+  public alert = false;
+
   // tslint:disable-next-line: max-line-length
   public newFence: IFence = { id: undefined, homeOwner: '', address: '', date: new Date(), builder: '', feetOfFence: undefined, heightOfFence: undefined, typeOfFence: '', gates: undefined, curb: true, stain: true, bOrC: '', price: undefined };
 
-  constructor(private fenceService: FenceService) { }
+  constructor(
+    private fenceService: FenceService,
+    private router: Router) { }
 
   ngOnInit() {
   }
 
   async save() {
     await this.fenceService.addFence(this.newFence);
+  }
+
+  back() {
+    this.router.navigate(['fence']);
+  }
+
+  alerting() {
+    this.alert = !this.alert;
   }
 
 }
