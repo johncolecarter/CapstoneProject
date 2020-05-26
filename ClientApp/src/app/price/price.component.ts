580475719e73a4db0b1832b86f19794a4a6dc4cc
johncolecarter <colecarter7777@gmail.com>
2020-05-26 09:51:51 -0500

    updated price estimator

--- ClientApp/src/app/price/price.component.ts
+++ ClientApp/src/app/price/price.component.ts
@@ -28,19 +28,131 @@ export class PriceComponent implements OnInit {
 
         } else if (this.price.heightOfFence === 6) {
 
-          this.total = (this.price.feetOfFence * 20) + (this.price.gates * 250) + (this.price.extra + 2000);
+          this.total = (this.price.feetOfFence * 20) + (this.price.gates * 250) + (this.price.extra);
 
         } else if (this.price.heightOfFence === 8) {
 
-          this.total = (this.price.feetOfFence * 40) + (this.price.gates * 250) + (this.price.extra + 2000);
+          this.total = (this.price.feetOfFence * 40) + (this.price.gates * 250) + (this.price.extra);
 
         } else if (this.price.heightOfFence === 9) {
 
-          this.total = (this.price.feetOfFence * 45) + (this.price.gates * 250) + (this.price.extra + 2000);
+          this.total = (this.price.feetOfFence * 45) + (this.price.gates * 250) + (this.price.extra);
 
         } else if (this.price.heightOfFence === 10) {
 
-          this.total = (this.price.feetOfFence * 50) + (this.price.gates * 250) + (this.price.extra + 2000);
+          this.total = (this.price.feetOfFence * 50) + (this.price.gates * 250) + (this.price.extra);
+
+        }
+        break;
+      }
+      case this.price.typeOfFence = 'Board on Board': {
+        if (this.price.heightOfFence === 7) {
+
+          this.total = (this.price.feetOfFence * 35) + (this.price.gates * 250) + (this.price.extra);
+
+        } else if (this.price.heightOfFence === 6) {
+
+          this.total = (this.price.feetOfFence * 25) + (this.price.gates * 250) + (this.price.extra);
+
+        } else if (this.price.heightOfFence === 8) {
+
+          this.total = (this.price.feetOfFence * 45) + (this.price.gates * 250) + (this.price.extra);
+
+        } else if (this.price.heightOfFence === 9) {
+
+          this.total = (this.price.feetOfFence * 50) + (this.price.gates * 250) + (this.price.extra);
+
+        } else if (this.price.heightOfFence === 10) {
+
+          this.total = (this.price.feetOfFence * 55) + (this.price.gates * 250) + (this.price.extra);
+
+        }
+        break;
+      }
+      case this.price.typeOfFence = 'Dog Eared': {
+        if (this.price.heightOfFence === 7) {
+
+          this.total = (this.price.feetOfFence * 25) + (this.price.gates * 100) + (this.price.extra);
+
+        } else if (this.price.heightOfFence === 6) {
+
+          this.total = (this.price.feetOfFence * 20) + (this.price.gates * 100) + (this.price.extra);
+
+        } else if (this.price.heightOfFence === 8) {
+
+          this.total = (this.price.feetOfFence * 30) + (this.price.gates * 100) + (this.price.extra);
+
+        }
+        break;
+      }
+      case this.price.typeOfFence = 'Horizontal': {
+        if (this.price.heightOfFence === 7) {
+
+          this.total = (this.price.feetOfFence * 40) + (this.price.gates * 250) + (this.price.extra);
+
+        } else if (this.price.heightOfFence === 6) {
+
+          this.total = (this.price.feetOfFence * 30) + (this.price.gates * 250) + (this.price.extra);
+
+        } else if (this.price.heightOfFence === 8) {
+
+          this.total = (this.price.feetOfFence * 45) + (this.price.gates * 250) + (this.price.extra);
+
+        } else if (this.price.heightOfFence === 9) {
+
+          this.total = (this.price.feetOfFence * 50) + (this.price.gates * 250) + (this.price.extra);
+
+        } else if (this.price.heightOfFence === 10) {
+
+          this.total = (this.price.feetOfFence * 55) + (this.price.gates * 250) + (this.price.extra);
+
+        }
+        break;
+      }
+      case this.price.typeOfFence = 'Horizontal BOB': {
+        if (this.price.heightOfFence === 7) {
+
+          this.total = (this.price.feetOfFence * 45) + (this.price.gates * 250) + (this.price.extra);
+
+        } else if (this.price.heightOfFence === 6) {
+
+          this.total = (this.price.feetOfFence * 35) + (this.price.gates * 250) + (this.price.extra);
+
+        } else if (this.price.heightOfFence === 8) {
+
+          this.total = (this.price.feetOfFence * 50) + (this.price.gates * 250) + (this.price.extra);
+
+        } else if (this.price.heightOfFence === 9) {
+
+          this.total = (this.price.feetOfFence * 55) + (this.price.gates * 250) + (this.price.extra);
+
+        } else if (this.price.heightOfFence === 10) {
+
+          this.total = (this.price.feetOfFence * 60) + (this.price.gates * 250) + (this.price.extra);
+
+        }
+        break;
+      }
+      case this.price.typeOfFence = 'Regular w/ 4in Overlay': {
+        if (this.price.heightOfFence === 7) {
+
+          this.total = (this.price.feetOfFence * 35) + (this.price.gates * 250) + (this.price.extra);
+
+        } else if (this.price.heightOfFence === 6) {
+
+          this.total = (this.price.feetOfFence * 25) + (this.price.gates * 250) + (this.price.extra);
+
+        } else if (this.price.heightOfFence === 8) {
+
+          this.total = (this.price.feetOfFence * 45) + (this.price.gates * 250) + (this.price.extra);
+
+        } else if (this.price.heightOfFence === 9) {
+
+          this.total = (this.price.feetOfFence * 50) + (this.price.gates * 250) + (this.price.extra);
+
+        } else if (this.price.heightOfFence === 10) {
+
+          this.total = (this.price.feetOfFence * 55) + (this.price.gates * 250) + (this.price.extra);
 
         }
         break;
